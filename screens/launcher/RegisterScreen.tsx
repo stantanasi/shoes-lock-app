093f6effd2f9652bd0ccd7335d83b7cfe3a77a0d
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-09 15:42:43 +0100

    feat(register): implement create user using Firebase auth

--- screens/launcher/RegisterScreen.tsx
+++ screens/launcher/RegisterScreen.tsx
@@ -1,10 +1,29 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
+import { auth, fireDB } from '../../firebase';
 
 export default function RegisterScreen() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const register = (name: string, email: string, password: string) => {
+    auth.createUserWithEmailAndPassword(email, password)
+      .then(async (credentials) => {
+        const user = credentials.user;
+        if (!user) throw new Error('User is null');
+
+        await fireDB
+          .collection('user')
+          .add({
+            uid: user.uid,
+            name: name,
+          });
+
+        return user;
+      })
+      .catch((error) => console.log(error));
+  }
 
   return (
     <View style={styles.container}>
@@ -26,6 +45,16 @@ export default function RegisterScreen() {
         style={styles.password}
         value={password}
         onChangeText={(value) => setPassword(value)}
+      />
+      <Button
+        title='Sign In'
+        onPress={() => {
+          if (name.trim() === '') return;
+          if (email.trim() === '') return;
+          if (password.trim() === '') return;
+
+          register(name, email, password);
+        }}
       />
     </View>
   )
