7e2c88ea381fde97dc78ce90846807f61623e005
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-09 15:43:18 +0100

    feat(login): implement form to login

--- screens/launcher/LoginScreen.tsx
+++ screens/launcher/LoginScreen.tsx
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        placeholder='Email'
+        style={styles.email}
+        value={email}
+        onChangeText={(value) => setEmail(value)}
+      />
+      <TextInput
+        placeholder='Password'
+        style={styles.password}
+        value={password}
+        onChangeText={(value) => setPassword(value)}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {},
+  email: {},
+  password: {},
+})
