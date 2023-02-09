9f7698600a9a30f27dd11642fe4d18ffc5cff217
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-09 15:18:18 +0100

    feat: create LauncherScreen

---
+++ screens/launcher/LauncherScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function LauncherScreen() {
+  return (
+    <View>
+      <Text>LauncherScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
