8b229b4c6c4b33700336cc9e28b141b21320bdb3
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-09 15:43:48 +0100

    feat(login): implement login using Firebase auth

--- screens/launcher/LoginScreen.tsx
+++ screens/launcher/LoginScreen.tsx
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
+import { auth } from '../../firebase';
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const login = (email: string, password: string) => {
+    auth.signInWithEmailAndPassword(email, password)
+      .then((credentials) => {
+        const user = credentials.user;
+
+        return user;
+      })
+      .catch((error) => console.error(error));
+  }
 
   return (
     <View style={styles.container}>
@@ -19,6 +30,15 @@ export default function LoginScreen() {
         style={styles.password}
         value={password}
         onChangeText={(value) => setPassword(value)}
+      />
+      <Button
+        title='Login'
+        onPress={() => {
+          if (email.trim() === '') return;
+          if (password.trim() === '') return;
+
+          login(email, password);
+        }}
       />
     </View>
   )
