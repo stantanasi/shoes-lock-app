fdeec11f912bb57b400f3dcc9a437b7698f9242f
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-16 16:31:28 +0100

    feat(profile-settings): implement logout

--- screens/profile/ProfileSettingsScreen.tsx
+++ screens/profile/ProfileSettingsScreen.tsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { RootStackScreenProps } from '../../types'
 
-export default function ProfileSettingsScreen() {
+export default function ProfileSettingsScreen({ navigation }: RootStackScreenProps<'ProfileSettings'>) {
+  const logout = () => {
+    AsyncStorage.setItem('user', '')
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'Launcher' }],
+        });
+      })
+  }
+
   return (
-    <View>
-      <Text>ProfileSettingsScreen</Text>
+    <View style={styles.container}>
+      <Button
+        title="Logout"
+        onPress={() => logout()}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
